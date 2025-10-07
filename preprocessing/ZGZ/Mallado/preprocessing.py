872282fbf1057f9bef283c4ba2a0ef5344fa51c5
alexisimo <alexis0023@hotmail.com>
2025-10-07 17:27:08 +0200

    GHG calculation moved to pre-processing by dataset

--- preprocessing/ZGZ/Mallado/preprocessing.py
+++ preprocessing/ZGZ/Mallado/preprocessing.py
@@ -1,8 +1,14 @@
 #%%
 import geopandas as gp
+import os
 
 path = "data/ZGZ/Mallado/raw/Mallado2024.zip"
 path_to = "data/ZGZ/Mallado/refined/Mallado2024.csv"
+
+# Check whether the specified path exists or not
+if not os.path.exists(os.path.dirname(path_to)):
+   # Create a new directory because it does not exist
+   os.makedirs(os.path.dirname(path_to))
 
 # Reading the compressed SHP file by attaching ! and the file name
 grid = gp.read_file(path+"!"+"Mallado2024.shp")
